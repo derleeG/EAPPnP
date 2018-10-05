b82b252ebdc2cb7e45de5c80df103abb3603e957
derleeG <color.ui>
2018-10-04 20:25:07 -0400

    basic torch implementation

--- src/EAPPnPSolverTorch.py
+++ src/EAPPnPSolverTorch.py
@@ -1,6 +1,5 @@
 # modified EPPnP algorithm to handle unknown scales in the 3D model
-import numpy as np
-from ..lib import pyprocrutes as procrutes
+import torch
 
 
 def EAPPnP(P, p):
@@ -13,24 +12,9 @@ def EAPPnP(P, p):
     M, Cw, Alph = prepare_data(cP, p)
     Km = kernel_noise(M, 4)
     R, T, S, err = generalized_kernel_PnP(Cw, Km)
-    T = T - np.matmul(R*S, np.reshape(mP, (-1, 1)))
+    T = T - (R*S).mm(mP.view(-1, 1))
 
     return R, T, S, err
-
-
-def EPPnP(P, p):
-    '''
-    P: nx3 matrix, points in world coordinates
-    p: nx2 matrix, points in camera coordinates
-    '''
-
-    cP, mP = centralize(P, 0)
-    M, Cw, Alph = prepare_data(cP, p)
-    Km = kernel_noise(M, 4)
-    R, T, err = kernel_PnP(Cw, Km)
-    T = T - np.matmul(R, np.reshape(mP, (-1, 1)))
-
-    return R, T, err
 
 
 def EAPPnPMCS(P, p, t):
@@ -45,115 +29,45 @@ def EAPPnPMCS(P, p, t):
     b = prepare_offset(p, -t)
     Km, Cc = kernel_noise_full(M, b, 4)
     R, T, S, err = generalized_kernel_PnP_MCS(Cw, Cc, Km)
-    T = T - np.matmul(R*S, np.reshape(mP, (-1, 1)))
+    T = T - (R*S).mm(mP.view(-1, 1))
 
     return R, T, S, err
 
 
-def EPPnPMCS(P, p, t):
-    pass
-
-
-def EAPPnL(L, l):
-    pass
-
-
-def EPPnL(L, l):
-    pass
-
-
-def EAPPnLMCS(L, l, t):
-    pass
-
-
-def EPPnLMCS(L, l, t):
-    pass
-
-
-def EAPPnX(P, p, L, l):
-    pass
-
-
-def EPPnX(P, p, L, l):
-    pass
-
-
-def EAPPnXMCS(P, p, tp, L, l, tl):
-    pass
-
-
-def EPPnXMCS(P, p, tp, L, l, tl):
-    pass
-
-
 def prepare_data(P, p):
 
-    Cw = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1], [0, 0, 0]], dtype=np.float32)
-    Alph = np.concatenate((P, 1-np.sum(P, -1, keepdims=True)), -1)
-    _Alph = np.reshape(Alph, (-1, 1))
-    _Alph_u = np.reshape((Alph*np.reshape(p[:,0], (-1, 1))), (-1, 1))
-    _Alph_v = np.reshape((Alph*np.reshape(p[:,1], (-1, 1))), (-1, 1))
+    Cw = torch.tensor([[1, 0, 0], [0, 1, 0], [0, 0, 1], [0, 0, 0]]).to(P)
+    Alph = torch.cat((P, 1-P.sum(-1, keepdim=True)), -1)
+    _Alph = Alph.view(-1, 1)
+    _Alph_u = (Alph*p[:,0].view(-1, 1)).view(-1, 1)
+    _Alph_v = (Alph*p[:,1].view(-1, 1)).view(-1, 1)
 
-    M = np.concatenate(
-            (np.reshape(np.concatenate((_Alph, np.zeros(_Alph.shape), -_Alph_u), -1), (-1, 12)),
-             np.reshape(np.concatenate((np.zeros(_Alph.shape), _Alph, -_Alph_v), -1), (-1, 12))),
-            0).astype(np.float32)
+    M = torch.cat(
+            (torch.cat((_Alph, torch.zeros_like(_Alph), -_Alph_u), -1).view(-1, 12),
+             torch.cat((torch.zeros_like(_Alph), _Alph, -_Alph_v), -1).view(-1, 12)),
+            0)
 
     return M, Cw, Alph
 
 
 def prepare_offset(p, t):
-    b = np.concatenate(
-            (np.reshape(p[:, 0]*t[:, 2] - t[:, 0], (-1, 1)),
-             np.reshape(p[:, 1]*t[:, 2] - t[:, 1], (-1, 1))),
-            0).astype(np.float32)
+    b = torch.cat(
+            ((p[:, 0]*t[:, 2] - t[:, 0]).view(-1, 1),
+             (p[:, 1]*t[:, 2] - t[:, 1]).view(-1, 1)),
+            0)
     return b
 
 
 def kernel_noise(M, dims):
-    _, _, Vt = np.linalg.svd(M, full_matrices=M.shape[0] < M.shape[1])
-    return Vt.T[:, -dims:]
+    _, _, V = torch.svd(M, some=M.shape[0] >= M.shape[1])
+    return V[:, -dims:]
 
 
 def kernel_noise_full(M, b, dims):
-    U, s, Vt = np.linalg.svd(M, full_matrices=M.shape[0] < M.shape[1])
-    Cc = np.matmul(np.matmul(b.T, U)/s, Vt[:s.size,:]).T
-    return Vt.T[:, -dims:], Cc
+    U, s, V = torch.svd(M, some=M.shape[0] >= M.shape[1])
 
-
-def kernel_PnP(Cw, Km, iter_num=10):
-    '''
-    find R, t and S such that ||RSCw + t - Cc||^2 is minimized
-    '''
-
-    X = Cw.T
-    cX, mX = centralize(X, -1)
-    Y = Km[:,-1].reshape(-1, 3).T
-    if Y[-1,:].mean() < 0:
-        # control points should be in front of the camera
-        Y = -Y
-
-    cY, mY = centralize(Y, -1)
-    Ynorm = np.linalg.norm(cY)
-    nY = cY/Ynorm
-    R, s = procrutes.isotropic_procrutes(cX, nY)
-    s *= Ynorm
-
-    for it in range(iter_num):
-        Y = np.matmul(R, cX) + mY/s
-
-        # project into the effective null space of M
-        Y = np.matmul(Km, np.matmul(Km.T, (Y.T).reshape(-1, 1))).reshape(-1, 3).T
-        newerr = np.linalg.norm(np.matmul(R.T, Y-Y.mean(-1, keepdims=True)) - cX)
-        if it > 1 and newerr > err*0.95:
-            break
-        err = newerr
-        cY, mY = centralize(Y, -1)
-        R, s = procrutes.isotropic_procrutes(cX, cY)
-
-    T = mY/s - np.matmul(R, mX)
-
-    return R, T, err
+    Cc = (b.t().mm(U)/s).mm(V.t()[:s.numel(),:]).t()
+    return V[:, -dims:], Cc
 
 
 def generalized_kernel_PnP(Cw, Km, iter_num=10):
@@ -161,26 +75,26 @@ def generalized_kernel_PnP(Cw, Km, iter_num=10):
     find R, t and S such that ||RSCw + t - Cc||^2 is minimized
     '''
 
-    X = Cw.T
+    X = Cw.t()
     cX, mX = centralize(X, -1)
-    Y = Km[:,-1].reshape(-1, 3).T
+    Y = Km[:,-1].reshape(-1, 3).t()
     if Y[-1,:].mean() < 0:
         # control points should be in front of the camera
         Y = -Y
 
     cY, mY = centralize(Y, -1)
-    Ynorm = np.linalg.norm(cY)
+    Ynorm = torch.norm(cY)
     nY = cY/Ynorm
     R, S = procrutes.anisotropic_procrutes(cX, nY)
     S *= Ynorm
 
     for it in range(iter_num):
-        scale = np.cbrt(np.prod(S))
-        Y, S = (np.matmul(R*S, cX) + mY)/scale, S/scale
+        scale = torch.prod(S).pow(1/3)
+        Y, S = ((R*S).mm(cX) + mY)/scale, S/scale
 
         # project into the effective null space of M
-        Y = np.matmul(Km, np.matmul(Km.T, (Y.T).reshape(-1, 1))).reshape(-1, 3).T
-        newerr = np.linalg.norm(np.matmul(R.T, Y-Y.mean(-1, keepdims=True))/S.reshape(-1, 1) - cX)
+        Y = Km.mm(Km.t().mm(Y.t().view(-1, 1))).reshape(-1, 3).t()
+        newerr = torch.norm(R.t().mm(Y-Y.mean(-1, keepdim=True))/S.view(-1, 1) - cX)
         if it > 1 and newerr > err*0.95:
             break
         err = newerr
@@ -188,8 +102,8 @@ def generalized_kernel_PnP(Cw, Km, iter_num=10):
         R, S = procrutes.anisotropic_procrutes(cX, cY, S, 5)
 
     R, S = procrutes.anisotropic_procrutes(cX, cY, S, 15)
-    scale = np.cbrt(np.prod(S))
-    T, S = (mY - np.matmul(R*S, mX))/scale, S/scale
+    scale = torch.prod(S).pow(1/3)
+    T, S = (mY - (R*S).mm(mX))/scale, S/scale
 
     return R, T, S, err
 
@@ -199,7 +113,7 @@ def generalized_kernel_PnP_MCS(Cw, Cc, Km, iter_num=10):
     find R, t and S such that ||RSCw + t - Cc||^2 is minimized
     '''
 
-    X = Cw.T
+    X = Cw.t()
     cX, mX = centralize(X, -1)
     Y = Cc.reshape(-1, 3).T
     if Y[-1,:].mean() < 0:
@@ -210,11 +124,12 @@ def generalized_kernel_PnP_MCS(Cw, Cc, Km, iter_num=10):
     R, S = procrutes.anisotropic_procrutes(cX, cY)
 
     for it in range(iter_num):
-        Y = np.matmul(R*S, cX) + mY
+        scale = torch.prod(S).pow(1/3)
+        Y, S = ((R*S).mm(cX) + mY)/scale, S/scale
 
         # project into the effective null space of M
-        Y = (np.matmul(Km, np.matmul(Km.T, (Y.T).reshape(-1, 1) - Cc)) + Cc).reshape(-1, 3).T
-        newerr = np.linalg.norm(np.matmul(R.T, Y-Y.mean(-1, keepdims=True))/S.reshape(-1, 1) - cX)
+        Y = (Km.mm(Km.t().mm(Y.t().view(-1, 1) - Cc)) + Cc).reshape(-1, 3).t()
+        newerr = torch.norm(R.t().mm(Y-Y.mean(-1, keepdim=True))/S.view(-1, 1) - cX)
         if it > 1 and newerr > err*0.95:
             break
         err = newerr
@@ -222,16 +137,44 @@ def generalized_kernel_PnP_MCS(Cw, Cc, Km, iter_num=10):
         R, S = procrutes.anisotropic_procrutes(cX, cY, S, 5)
 
     R, S = procrutes.anisotropic_procrutes(cX, cY, S, 15)
-    T = mY - np.matmul(R*S, mX)
+    scale = torch.prod(S).pow(1/3)
+    T, S = (mY - (R*S).mm(mX))/scale, S/scale
 
     return R, T, S, err
 
 
 def centralize(X, dim=None):
-    mX = X.mean(dim, keepdims=True)
+    mX = X.mean(dim, True)
     cX = X - mX
 
     return cX, mX
+
+
+class procrutes:
+    def anisotropic_procrutes(X, Y, S=None, iter_num=30):
+        A = Y.mm(X.t())
+        if S is None:
+            S = torch.ones(3).to(X)
+
+        Xs = (X*X).sum(-1)
+        for _ in range(iter_num):
+            R = procrutes.orthogonal_polar_factor(A*S)
+            S = (A*R).sum(0) / Xs
+
+        return R, S
+
+
+    def orthogonal_polar_factor(A):
+        U, S, V = torch.svd(A)
+        S = torch.ones_like(S)
+        D[-1] = U.mm(V.t()).det().sign()
+        R = (U*D).mm(V.t())
+
+        return R
+
+
+
+
 
 
 
